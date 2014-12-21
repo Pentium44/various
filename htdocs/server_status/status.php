cae80ccbe13cb86821dbc73be3d7c12bf0fd78ce
Pentium44 <cdorm245@gmail.com>
2014-12-21 00:22:17 -0800

    [server_status] now checks if server is online from protocol.

--- htdocs/server_status/status.php
+++ htdocs/server_status/status.php
@@ -1,11 +1,39 @@
 <?php
+# Simple script to check if mt / voxelands / freeminer server is online.
+# check_if_minetestserver_up was used from the protocol.txt file.
+
 $argv = $_SERVER['argv'];
 $ip = htmlentities(stripslashes($argv[1]));
 $port = htmlentities(stripslashes($argv[2]));
 #=======================================================================
 # functions
+function check_if_minetestserver_up($host, $port)
+{
+	$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+	$timeout = array("sec" => 1, "usec" => 0);
+	socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, $timeout);
+	$buf = "\x4f\x45\x74\x03\x00\x00\x00\x03\xff\xdc\x01";
+	socket_sendto($socket, $buf, strlen($buf), 0, $host, $port);
+	$buf = socket_read($socket, 1000);
+	if($buf != "")
+	{
+		# We got a reply! read the peer id from it.
+		$peer_id = substr($buf, 9, 2);
+		
+		# Disconnect
+		$buf = "\x4f\x45\x74\x03".$peer_id."\x00\x00\x03";
+		socket_sendto($socket, $buf, strlen($buf), 0, $host, $port);
+		socket_close($socket);
+		
+		# WOOO server's online :3
+		return 1;
+	}
+	# ah damn, server's offline :p
+	return 0;
+}
+
 function check($ip, $port) {
-	$check = @fsockopen($ip, $port, $errno, $errstr, 2);
+	$check = check_if_minetestserver_up(gethostbyname($ip),$port);
 	if (!$check)
 	{
 		gentext(0);
